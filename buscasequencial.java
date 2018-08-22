dd742f9b50018196e4f80faa9c025f65713c3f77
IsmaelRDC <42240015+IsmaelRDC@users.noreply.github.com>
2018-08-22 09:51:32 -0300

    Update buscasequencial.java

--- buscasequencial.java
+++ buscasequencial.java
@@ -1,36 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package bubllesort;
+
+package ativJava;
 import java.util.Scanner;
 import java.util.Random;
-/**
- *
- * @author Roberta
- */
-public class buscasequencial {
+
+public class buscaSequencial {
     public static void main(String[] args) {
-        Scanner tec = new Scanner(System.in);
-        Random alt = new Random();
+        Scanner tcl = new Scanner(System.in);
+        Random aleatorio = new Random();
         int vetor[] =new int[40];
-        boolean variav = false;
+        boolean variavel =true;
         for (int i = 0; i <vetor.length; i++) {
-            vetor[i] = alt.nextInt(100);
+            vetor[i] = aleatorio.nextInt(100);
         }
-        System.out.println("Diga os numeros que quer:");
-        int valor = tec.nextInt();
+        
+        System.out.println("Digite os numeros desejados:");
+        int valor = tcl.nextInt();
         for (int i = 0; i <vetor.length; i++) {
             if(valor == vetor[i]){
-                System.out.println("O numero foi encontrado");
-                System.out.println("o numero encontrado é"+ valor);
-                variav = true;
+                System.out.println("O numero que foi encontrado é"+ valor);
+                variavel = false;
                 break;
             }
         }
-        if (variav == false) {
-            System.out.println("O numero não foi encontrado");
+        
+        if (variavel == true) {
+            System.out.println("O numero digitado não foi encontrado :( ");
         }
     }
 }
